900b2e63fbcc390782eb1c3f233b7dc7ace3c496
Filip Štamcar <filip.stamcar@hotmail.com>
2025-12-20 10:46:22 +0100

    Add routes for the interaction stream

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,15 @@ async fn main() {
 
     let config = Config::new().expect("failed to load configuration");
 
+    let base_routes = routes::base_routes();
     let state_routes = routes::state_routes(config.token.clone());
+    let interaction_routes = routes::interaction_routes(config.token.clone());
 
-    let routes = state_routes.recover(rejection_handler).with(warp::trace::request());
+    let routes = base_routes
+        .or(state_routes)
+        .or(interaction_routes)
+        .recover(rejection_handler)
+        .with(warp::trace::request());
 
     tracing::info!("starting server on {}:{}", config.address, config.port);
     warp::serve(routes).run((config.address, config.port)).await;
--- src/routes.rs
+++ src/routes.rs
@@ -3,7 +3,18 @@ use std::sync::Arc;
 use tokio::sync::broadcast;
 use warp::Filter;
 
-use crate::utils::{StreamReceiverExt, StreamTransmitterExt, cors_any_origin, subprotocol_auth};
+use crate::utils::{
+    StreamReceiverExt,
+    StreamTransmitterExt,
+    authorization_auth,
+    cors_any_origin,
+    subprotocol_auth,
+};
+
+pub fn base_routes()
+-> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone + Send {
+    warp::path!("health").map(|| warp::reply::with_status("OK", warp::http::StatusCode::OK))
+}
 
 pub fn state_routes(
     token: Arc<String>,
@@ -17,6 +28,24 @@ pub fn state_routes(
 
     let receiver = warp::path!("state" / "push")
         .and(subprotocol_auth(token))
+        .and_receive_stream(channel.clone());
+
+    transmitter.or(receiver)
+}
+
+pub fn interaction_routes(
+    token: Arc<String>,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone + Send {
+    let (channel, _) = broadcast::channel::<String>(16);
+    let channel = Arc::new(channel);
+
+    let transmitter = warp::path!("interaction" / "stream")
+        .and(authorization_auth(token))
+        .and_transmit_stream(channel.clone())
+        .with(cors_any_origin());
+
+    let receiver = warp::path!("interaction" / "push")
+        // .and(subprotocol_auth(code))
         .and_receive_stream(channel.clone());
 
     transmitter.or(receiver)
